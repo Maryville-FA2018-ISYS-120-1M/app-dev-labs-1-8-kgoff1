3964a9bb96305c9cf9d55c28bb1ec5a3bcd1e48e
Kenneth Goff <kgoff1@live.maryville.edu>
2018-09-15 19:40:10 -0700

    homework

--- InterfaceBuilderBasics/ViewController.swift
+++ InterfaceBuilderBasics/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func myButton(_ sender: UIButton) { print("The button was pressed")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
