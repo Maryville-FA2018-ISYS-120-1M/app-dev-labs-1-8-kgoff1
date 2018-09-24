fbcc22dd87de71ba74d03d1b94d3d0280b122ed9
Shanae Ullman <shanae.ullman@gmail.com>
2018-09-23 22:43:55 -0500

    SU edits

--- InterfaceBuilderBasics/ViewController.swift
+++ InterfaceBuilderBasics/ViewController.swift
@@ -25,3 +25,27 @@ class ViewController: UIViewController {
 
 }
 
+/* Another option you could have used...
+ 
+ import UIKit
+ 
+ class ViewController: UIViewController {
+ 
+ @IBOutlet weak var mainLabel: UILabel!
+ @IBAction func changeTitle(_ sender: Any) {
+ mainLabel.text = "This app rocks!"
+ }
+ override func viewDidLoad() {
+ super.viewDidLoad()
+ // Do any additional setup after loading the view, typically from a nib.
+ }
+ 
+ override func didReceiveMemoryWarning() {
+ super.didReceiveMemoryWarning()
+ // Dispose of any resources that can be recreated.
+ }
+ 
+ 
+ }
+ 
+ */
